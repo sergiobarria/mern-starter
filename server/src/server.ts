7cd1d526cbea36b4e7f059492e86fa9f091df7b5
sergiobarria <sbarria.dev@gmail.com>
2023-03-05 01:00:28 -0500

    chore: express server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,21 @@
-console.log('Hello World');
+import * as http from 'http'
+
+import { app } from './app'
+
+const PORT = Number(process.env.PORT ?? 3000)
+
+let server: http.Server
+
+const startServer = async (): Promise<void> => {
+  server = http.createServer(app)
+
+  try {
+    server.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server listening on port ${PORT}`)
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+void startServer()
